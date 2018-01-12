ab1399147a0b58e1076eecd0497bc6bff894ef21
github username <github email address>
2018-01-12 15:18:40 -0500

    Login front end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
       </div>
     );
   }
