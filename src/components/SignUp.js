a2f7d80436ce7fece857cc75c791362a405a322b
github username <github email address>
2018-01-12 14:30:09 -0500

    Added basic foundation for redux

---
+++ src/components/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input />
+          <input type="password"/>
+          <button type="submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // for routes
+import { createStore, applyMiddleware } from 'redux'; // store for redux state, middleware for async ajax requests
+import { Provider } from 'react-redux'; // lets react have access to redux store
+import thunk from 'redux-thunk'; // middleware for async ajax requests
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}> // wrap whole thing in Provider so it has access to store
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
